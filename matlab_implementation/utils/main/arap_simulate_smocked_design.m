7a0f42e5f1e8f01c11dc643b431a62251aa1ae46
Jing Ren <jing.ren@kaust.edu.sa>
2023-01-25 21:50:23 +0100

    Update arap_simulate_smocked_design.m

--- matlab_implementation/utils/main/arap_simulate_smocked_design.m
+++ matlab_implementation/utils/main/arap_simulate_smocked_design.m
@@ -1,17 +1,21 @@
-function [F, U, UV, grid_V, vid, V_new, F_quad] = arap_simulate_smocked_design(SP, SG, X, grid_step, eps_node, margin)
+function [F, U, UV, grid_V, vid, V_new, F_quad] = arap_simulate_smocked_design(SP, SG, X, ...
+    grid_step, ...
+    eps_node, ...
+    margin,...
+    ifsetboundryfree)
+
 if nargin < 5, eps_node = 0; end
 
-% [gx, gy]= meshgrid(-2*grid_step:grid_step:SP.len_x+2*grid_step,...
-%     -2*grid_step:grid_step:SP.len_y+2*grid_step);
-
-
-[gx, gy]= meshgrid(-0.5:grid_step:SP.len_x+0.5,...
-    -0.5:grid_step:SP.len_y+0.5);
-if nargin > 5
-    [gx, gy]= meshgrid(-margin(1)+min(SP.grid_V(:,1)):grid_step:max(SP.grid_V(:,1))+margin(2),...
-    -margin(3)+min(SP.grid_V(:,2)):grid_step:max(SP.grid_V(:,2))+margin(4));
-
+if nargin < 6
+    margin = [0.5, 0.5, 0.5, 0.5];
 end
+
+if nargin < 7, ifsetboundryfree = false; end
+
+
+
+[gx, gy]= meshgrid(-margin(1)+min(SP.grid_V(:,1)):grid_step:max(SP.grid_V(:,1))+margin(2),...
+    -margin(3)+min(SP.grid_V(:,2)):grid_step:max(SP.grid_V(:,2))+margin(4));
 
 [V, ~, F_quad, ~] = extract_graph_from_meshgrid(gx, gy);
 
@@ -43,13 +47,15 @@ if eps_node > 0 %
     end
 end
 
-vid_ori = cell2mat(SG.vid_from_sp(SG.vid_pleat));
-vid_boundary = vid_ori([find(SP.V(vid_ori, 1) == min(SP.V(:,1))); ...
-    find(SP.V(vid_ori, 1) == max(SP.V(:, 1)));...
-    find(SP.V(vid_ori, 2) == min(SP.V(:,2)));...
-    find(SP.V(vid_ori, 2) == max(SP.V(:,2)));]);
-vid(vid_boundary) = [];
-V_new(vid_boundary, :) = [];
+if ifsetboundryfree
+    vid_ori = cell2mat(SG.vid_from_sp(SG.vid_pleat));
+    vid_boundary = vid_ori([find(SP.V(vid_ori, 1) == min(SP.V(:,1))); ...
+        find(SP.V(vid_ori, 1) == max(SP.V(:, 1)));...
+        find(SP.V(vid_ori, 2) == min(SP.V(:,2)));...
+        find(SP.V(vid_ori, 2) == max(SP.V(:,2)));]);
+    vid(vid_boundary) = [];
+    V_new(vid_boundary, :) = [];
+end
 
 
 U = arap(V, F, vid, V_new);
